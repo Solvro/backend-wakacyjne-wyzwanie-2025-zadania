428111d8cf358aeb9c31721ffa241c6384051eaa
patmik <patmikdev@gmail.com>
2025-08-02 21:05:06 +0200

    feat: created /wakacyjne route and /wakacyjne/backend. Set HttpCode and correct output for routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpCode } from "@nestjs/common";
 
-@Controller()
+import { AppService } from "./app.service";
+
+@Controller("wakacyjne")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("backend")
+  @HttpCode(418)
+  getHello(): { title: string; quote: string } {
+    return {
+      title: "Wakacyjne Wyzwanie Solvro!!!",
+      quote: "Jakaś twoja złota myśl - liczę na kreatywność",
+    };
   }
 }
