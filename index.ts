9494b1ca097519d739fbc26c7227ead3d1874528
patmik <patmikdev@gmail.com>
2025-08-02 19:00:29 +0200

    feat: added function to write a message into a file given amount of times

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { sys } from "typescript";
+
+function eloZelo(numofrec:number): void {
+    let text: string = "";
+    if (numofrec < 0) {
+        throw new Error("Number of records cannot be negative");
+    }
+    while (numofrec > 0) {
+        text += "Elo żelo\n";
+        numofrec--;
+    }   
+    if (text.length > 0) {
+        text.endsWith("\n") ? text = text.slice(0, -1) : null;
+    }
+    sys.writeFile("elo-żelo.txt", text);
+}
