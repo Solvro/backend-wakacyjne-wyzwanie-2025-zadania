09ecbc54e7d6a8aff0243bf261da50297a9a2e33
devilian1993 <maksioczko@gmail.com>
2025-08-04 16:49:46 +0200

    fix: remove unnecessary for loop

--- index.ts
+++ index.ts
@@ -4,9 +4,7 @@ function eloZelo(repetitions: number): void {
     const sentence: string = "Elo żelo\n";
     const content: string = sentence.repeat(repetitions);
 
-    for(let i = 0; i < repetitions; i++) {
-        writeFileSync("elo-zelo.txt", content);
-    }
+    writeFileSync("elo-zelo.txt", content);
 }
 
 const date: Date = new Date();
