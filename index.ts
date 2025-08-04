008edaf848a52d780dbda6b8f7ff3a4a22002353
Kajta <kajtakamil1@gmail.com>
2025-08-04 20:08:08 +0200

    solution to task 0

--- index.ts
+++ index.ts
@@ -1,14 +1,11 @@
-import * as fs from 'fs'
+import * as fs from "fs";
 
-function eloZelo(numberOfLines :number):void{
-    
-    const text="Elo żelo\n";
-    for (let index = 0; index < numberOfLines; index++) {
-        fs.appendFileSync('elo-żelo.txt',text);
-    }
-
-
+function eloZelo(numberOfLines: number): void {
+  const text = "Elo żelo\n";
+  for (let index = 0; index < numberOfLines; index++) {
+    fs.appendFileSync("elo-żelo.txt", text);
+  }
 }
 
-const now= new Date();
+const now = new Date();
 eloZelo(now.getMinutes());
