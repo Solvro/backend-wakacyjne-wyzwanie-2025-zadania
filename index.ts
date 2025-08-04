dc9eeea66b120dcf674218e7394124c229e44a81
reksion <janjuskowiak2004@gmail.com>
2025-08-04 11:03:53 +0200

    init

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { writeFileSync } from "fs"
 
 function eloZelo(x: number): void {
   const line = "Elo żelo";
-  writeFileSync("elo-zelo.txt", Array(x).fill(line).join("\n"), "utf8");
+  writeFileSync("elo-żelo.txt", Array(x).fill(line).join("\n"), "utf8");
 }
 
 eloZelo((new Date()).getMinutes());
