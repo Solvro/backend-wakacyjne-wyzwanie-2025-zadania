18c48544a73c3fa132d6e3f6004c2bef8348f325
BKmil <bkm1l3k@gmail.com>
2025-08-04 18:01:03 +0200

    added index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as fs from "fs";
+
+function eloZelo(x: number): void {
+  const content = Array(x).fill("Elo żelo").join("\n");
+  fs.writeFileSync("elo-żelo.txt", content, "utf-8");
+}
+
+const d = new Date();
+const minutes = d.getMinutes();
+
+eloZelo(minutes);
