415f1c9e9866287003c223713754b05d2f751ee5
wsparcie <78564645+wsparcie@users.noreply.github.com>
2025-08-05 16:38:00 +0200

    refactor: - use single writeFileSync with previously created contents - remove console.logs

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
 import { writeFileSync } from 'fs'
 
 function eloZelo(repeats: number): void {
-  const text: string = 'Elo żelo'
-  const file: string = './elo-żelo.txt'
-  writeFileSync(file, '')
-  for (let i: number = 0; i < repeats; i++) {
-    writeFileSync(file, text + '\n', { flag: 'a' })
-    console.log(`${i + 1}: ${text}`)
-  }
+  const line: string = 'Elo żelo'
+  const file: string = 'elo-żelo.txt'
+  const contents: string = `${line}\n`.repeat(repeats)
+  writeFileSync(file, contents)
 }
 
-let minutes: number = new Date().getMinutes()
-console.log(`minutes: ${minutes}`)
-eloZelo(minutes)
+eloZelo(new Date().getMinutes())
