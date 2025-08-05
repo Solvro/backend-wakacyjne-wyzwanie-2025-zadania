6991d0045a4f382dfa6fe000cf29a9bf72ff176f
Jakub Morawiec <jakubmorawiec2@gmail.com>
2025-08-05 09:56:23 +0200

    fix: changed the code to be fully synchronous

--- 0. Wstęp/intex.ts
+++ 0. Wstęp/intex.ts
@@ -1,14 +1,16 @@
 import fs from 'fs';
 
 function eloZelo(times: number): void {
-    for (let i: number = 0; i <= times; i++) {
-            fs.appendFile('elo-żelo.txt', 'Elo żelo\n', 'utf8', (err) => {
-                if (err) console.error(err);
-            })
+    for (let i = 0; i <= times; i++) {
+        try {
+            fs.appendFileSync('elo-żelo.txt', 'Elo żelo\n', 'utf8');
+        } catch (err) {
+            console.error(err);
+        }
     }
 }
 
-const currDate: Date = new Date()
-let currMinutes: number = currDate.getMinutes()
+const currDate = new Date();
+const currMinutes = currDate.getMinutes();
 
-eloZelo(currMinutes)
+eloZelo(currMinutes);
