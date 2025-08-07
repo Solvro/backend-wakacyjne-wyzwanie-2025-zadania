50024936e142e265e37aafc6b0dad0335c0ea675
GitHubKameQ <90651206+kameqdev@users.noreply.github.com>
2025-08-07 14:38:04 +0200

    feat: added /wakacyjne/backend endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
+  getHello(): object {
+    return {
+      title: "Wakacyjne Wyzwanie Solvro!!!",
+      quote: "To mały krok dla człowieka, ale dla karła - normalny",
+    };
   }
 }
