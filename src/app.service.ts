80077fc1e3aca7b6b3a01f1e2202e71dfa7ade46
Krystian Śliwiński <72706877+Infor-Tech@users.noreply.github.com>
2025-08-09 23:03:59 +0200

    feat: task 1 solution

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): {title: string, quote: string} {
+    const message = {
+      title: "Wakacyjne Wyzwanie Solvro!!!",
+      quote: "francesinha (tost kiełbasa kotlet boczek parówki jakieś smażone mięso ser tost jajko sadzone - frytki i wszystko ratuje tłusty sos) :)"
+    }
+    return message;
   }
 }
