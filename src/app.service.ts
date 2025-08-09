f005b034ec3f92304ba34a00f663b9cc85248856
loudsheep <loudsheep05@gmail.com>
2025-08-09 16:43:43 +0200

    feat: return json from app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      title: "Wakacyjne Wyzwanie Solvro!!!",
+      quote: "Dziwne, u mnie działa ;)",
+    };
   }
 }
