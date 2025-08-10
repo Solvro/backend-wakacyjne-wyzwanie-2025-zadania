282a6f1e68c1cf59de43ccca1109ef4064b50362
JaroDomcio <281053@student.pwr.edu.pl>
2025-08-10 12:54:05 +0200

    feat: add solution for task 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
+  getHello() {
+    const message = {
+      title: "Wakacyjne Wyzwanie Solvro!!!",
+      quote: "Nie w4rto",
+    };
+    return message;
   }
 }
