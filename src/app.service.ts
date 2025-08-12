75cb7024b91b1e539cb10adf55214402bca4a0fd
marie <marie@firefly14G10a.uk-net>
2025-08-12 20:28:53 +0200

    feat: correct lint error 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): JSON {
-    const title: any = {
+    const title: JSON = {
       title: "Wakacyjne Wyzwanie Solvro!!!",
       quote:
         "There is a rumour going around that I have found God. I think this is unlikely because I have enough difficulty finding my keys, and there is empirical evidence that they exist. - Sir Terry Pratchett",
