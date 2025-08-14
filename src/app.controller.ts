336f0c8163e3979dabbd2c9aa2bf7e4afae24751
reksion <janjuskowiak2004@gmail.com>
2025-08-14 19:00:17 +0200

    chore: add prism

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,29 @@
 import { Controller, Get, HttpCode } from "@nestjs/common";
-
+import { DbService } from "./db.service";
 import { AppService } from "./app.service";
 
-@Controller("wakacyjne")
+@Controller("api/v1/")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly dbService: DbService) {}
 
-  @Get("backend")
+  @Get("wakacyjne/backend")
   @HttpCode(418)
   getBackend(): { title: string; quote: string } {
     return this.appService.getBackend();
+  }
+
+  @Get("db/test/show")
+  async getDbTest(): Promise<unknown> {
+    return await this.dbService.getTest();
+  }
+
+  @Get("db/test/create")
+  async getDbTestCreate(): Promise<void> {
+    return await this.dbService.getTestCreate();
+  }
+
+  @Get("db/test/clear")
+  async getDbTestClear(): Promise<void> {
+    return await this.dbService.getTestClear();
   }
 }
