8be7f12c61d7f2be6fb7281c32e3115135fd328b
Drewienkopl <hubert.drewienko@gmail.com>
2025-08-17 06:32:14 +0200

    feat: last test if after this it doesnt' work I give up

--- src/trip/trip.controller.spec.ts
+++ src/trip/trip.controller.spec.ts
@@ -2,19 +2,32 @@ import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
 
 import { TripController } from "./trip.controller";
+import { TripService } from "./trip.service";
 
 describe("TripController", () => {
-  let controller: TripController;
+  let tripController: TripController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TripController],
+      providers: [
+        {
+          provide: TripService,
+          useValue: {
+            getAllTrips: jest.fn().mockReturnValue([]),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<TripController>(TripController);
+    tripController = module.get<TripController>(TripController);
   });
 
-  it("should be defined", () => {
-    expect(controller).toBeDefined();
+  describe("root", () => {
+    it("should return like trips or something", () => {
+      expect(tripController.getAllTrips()).toStrictEqual(
+        tripController.getAllTrips(),
+      );
+    });
   });
 });
