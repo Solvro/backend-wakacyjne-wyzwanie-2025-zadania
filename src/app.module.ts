104754f16b8bfc39832799bcaa61577303991ebf
Karol Stolarczyk <k.stolar2005@gmail.com>
2025-08-18 17:46:45 +0200

    fix: warn prettier check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-
 import { DatabaseService } from "./database/database.service";
 import { TripController } from "./trip/trip.controller";
 import { TripService } from "./trip/trip.service";
