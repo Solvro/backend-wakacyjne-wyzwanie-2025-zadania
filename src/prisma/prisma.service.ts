75a31018530b45167c8c636b1d1111d593e36cb8
Dawid Błaszczyk <dawidbv1@gmail.com>
2025-08-19 17:45:51 +0200

    feat: integrate Prisma ORM and set up database schema
    - Added Prisma as a dependency in package.json and package-lock.json.
    - Created PrismaModule and PrismaService for managing database connections.
    - Defined database schema in schema.prisma with models for Trip, Expense, Participant, and ExpenseParticipant.
    - Created migration files for initial database setup including enums and tables.
    - Implemented seeding script to populate the database with initial data.
    - Updated .env.example to include DATABASE_URL for database connection.
    - Added generated Prisma client path to .gitignore.

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { PrismaClient } from "@prisma/client";
+
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
