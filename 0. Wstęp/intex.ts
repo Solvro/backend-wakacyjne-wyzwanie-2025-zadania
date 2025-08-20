37c72e7d0571f3e84f59070736e5f2f07f3c2767
jmorawiecpwr <jakubmorawiec2@gmail.com>
2025-08-20 10:21:15 +0200

    chore(format): apply prettier

--- 0. Wstęp/intex.ts
+++ 0. Wstęp/intex.ts
@@ -1,13 +1,13 @@
-import fs from 'fs';
+import fs from "fs";
 
 function eloZelo(times: number): void {
-    for (let i = 0; i <= times; i++) {
-        try {
-            fs.appendFileSync('elo-żelo.txt', 'Elo żelo\n', 'utf8');
-        } catch (err) {
-            console.error(err);
-        }
+  for (let i = 0; i <= times; i++) {
+    try {
+      fs.appendFileSync("elo-żelo.txt", "Elo żelo\n", "utf8");
+    } catch (err) {
+      console.error(err);
     }
+  }
 }
 
 const currDate = new Date();
