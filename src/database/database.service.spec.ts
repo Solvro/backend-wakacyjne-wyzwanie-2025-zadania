9149230efb2f1d16fae1736fee4c2cf424d17944
Jan Druszcz <jan.druszcz@onet.eu>
2025-08-22 19:32:56 +0200

    refactor: update files by prettier

--- src/database/database.service.spec.ts
+++ src/database/database.service.spec.ts
@@ -1,7 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { DatabaseService } from './database.service';
+import type { TestingModule } from "@nestjs/testing";
+import { Test } from "@nestjs/testing";
+import { DatabaseService } from "./database.service";
 
-describe('DatabaseService', () => {
+describe("DatabaseService", () => {
   let service: DatabaseService;
 
   beforeEach(async () => {
@@ -12,7 +13,7 @@ describe('DatabaseService', () => {
     service = module.get<DatabaseService>(DatabaseService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
