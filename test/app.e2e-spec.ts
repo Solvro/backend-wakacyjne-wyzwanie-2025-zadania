a4db4ce2a88a6c778ed0c553273dbac7d1c6e692
Harukume <harusume1001@gmail.com>
2025-08-24 23:57:01 +0200

    chore: fix e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,7 +18,7 @@ describe("AppController (e2e)", () => {
     await app.init();
   });
 
-  it.skip("/ (GET)", () => {
+  it("/ (GET)", () => {
     expect(app.getHttpServer()).toBeDefined();
   });
 });
