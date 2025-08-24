aee82c621aba6dc9955d66671f3cab39c344a9a1
Drewienkopl <hubert.drewienko@gmail.com>
2025-08-24 08:14:47 +0200

    fix: trying to fix errors in lint

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,10 +7,4 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
-
-  //   async enableShutdownHooks(app: INestApplication): Promise<void> {
-  //       this.$on('beforeExit', async () => {
-  //           await app.close();
-  //       });
-  //   }
 }
