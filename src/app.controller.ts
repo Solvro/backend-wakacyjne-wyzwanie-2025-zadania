4913de6ca299a93b4d23122e09f35c3fff22b8d0
patmik <patmikdev@gmail.com>
2025-08-24 17:28:48 +0200

    fix: add missing swagger decorators and fix tsconf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get, HttpCode } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 import { AppService } from "./app.service";
 
 @Controller("wakacyjne")
+@ApiTags("wakacyjne")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get("backend")
   @HttpCode(418)
+  @ApiOperation({
+    summary: "Get test api response",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Returns prepared api response for the sake of testing",
+  })
   getHello(): {
     title: string;
     quote: string;
--- src/prisma/prisma.controller.ts
+++ src/prisma/prisma.controller.ts
@@ -1,17 +1,29 @@
 import { Controller, Get, Put } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 import { PrismaService } from "./prisma.service";
 
 @Controller("database")
+@ApiTags("database")
 export class PrismaController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get("trips")
+  @ApiResponse({
+    status: 200,
+    description: "List of trips retrieved successfully",
+  })
+  @ApiOperation({ summary: "Get all trips" })
   async getTrips() {
     return await this.prisma.trip.findMany();
   }
 
   @Put("trips")
+  @ApiResponse({
+    status: 200,
+    description: "Example Trip created successfully",
+  })
+  @ApiOperation({ summary: "Create a test trip" })
   async testCreateTrips() {
     return await this.prisma.trip.create({
       data: {
@@ -26,10 +38,20 @@ export class PrismaController {
   }
 
   @Get("participants")
+  @ApiResponse({
+    status: 200,
+    description: "List of participants retrieved successfully",
+  })
+  @ApiOperation({ summary: "Get all participants" })
   async getParticipants() {
     return await this.prisma.participant.findMany();
   }
   @Get("expenses")
+  @ApiResponse({
+    status: 200,
+    description: "List of expenses retrieved successfully",
+  })
+  @ApiOperation({ summary: "Get all expenses" })
   async getExpenses() {
     return await this.prisma.expense.findMany();
   }
