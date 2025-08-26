cb2146ce9421452cac2fd0ab5ddf9fc0f69538ad
Jakub <60443955+jakubkobus@users.noreply.github.com>
2025-08-26 20:31:53 +0200

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { RequestMethod } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Wakacyjne wyzwanie")
+    .setDescription("Budzetownik backend API documentation")
+    .setVersion("1.0")
+    .setOpenAPIVersion("3.1.1")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, documentFactory);
 
   app.setGlobalPrefix("api/v1/", {
     exclude: [
