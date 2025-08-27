8939eaaea85bbb71ca599b5350820caf8f6c9768
Jakub <60443955+jakubkobus@users.noreply.github.com>
2025-08-27 15:28:28 +0200

    chore: use swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, HttpCode } from "@nestjs/common";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 
 import { AppService } from "./app.service";
 
+@ApiTags("wakacyjne")
 @Controller("wakacyjne")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get("backend")
   @HttpCode(418)
+  @ApiOperation({ description: "Returns the result of Task1" })
+  @ApiOkResponse({ description: "Successful response", type: Object })
   Task1(): object {
     return this.appService.Task1();
   }
