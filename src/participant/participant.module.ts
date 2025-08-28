963c772a988953408737e842eb992af70248dd2a
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-08-28 22:05:21 +0200

    chore: connect db to participant module

--- src/participant/participant.module.ts
+++ src/participant/participant.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 
+import { DatabaseModule } from "../database/database.module";
 import { ParticipantController } from "./participant.controller";
 import { ParticipantService } from "./participant.service";
 
 @Module({
   controllers: [ParticipantController],
   providers: [ParticipantService],
+  imports: [DatabaseModule],
 })
 export class ParticipantModule {}
