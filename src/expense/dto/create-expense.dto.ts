00fcb1aa376964c4cace7121df7a3f4b3f52037d
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-08-30 01:15:35 +0200

    feat: crud for expenses

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,1 +1,10 @@
-export class CreateExpenseDto {}
+import { ExpenseCategory } from "@prisma/client";
+
+export class CreateExpenseDto {
+  description: string;
+  amount: number;
+  category: ExpenseCategory;
+  date?: string;
+  tripId: number;
+  paidByParticipantId: number;
+}
--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,27 +1,115 @@
-import { Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 
+import { DatabaseService } from "../database/database.service";
 import { CreateExpenseDto } from "./dto/create-expense.dto";
 import { UpdateExpenseDto } from "./dto/update-expense.dto";
 
 @Injectable()
 export class ExpenseService {
-  create(createExpenseDto: CreateExpenseDto) {
-    return "This action adds a new expense";
+  constructor(private database: DatabaseService) {}
+
+  async create(createExpenseDto: CreateExpenseDto) {
+    const { tripId, paidByParticipantId } = createExpenseDto;
+
+    const trip = await this.database.trip.findUnique({
+      where: { id: tripId },
+      include: { participants: true },
+    });
+
+    if (trip === null) {
+      throw new NotFoundException(`Not found trip with ID: ${tripId}`);
+    }
+
+    const isParticipantInTrip = trip.participants.some(
+      (p) => p.id === paidByParticipantId,
+    );
+
+    if (!isParticipantInTrip) {
+      throw new BadRequestException(
+        `Participant with ID: ${paidByParticipantId} is not part of Trip with ID: ${tripId}`,
+      );
+    }
+
+    return this.database.expense.create({
+      data: createExpenseDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all expense`;
+  async findAll() {
+    return this.database.expense.findMany({
+      select: {
+        id: true,
+        description: true,
+        amount: true,
+        date: true,
+        category: true,
+        trip: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        paidBy: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} expense`;
+  async findOne(id: number) {
+    const expense = await this.database.expense.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        description: true,
+        amount: true,
+        date: true,
+        category: true,
+        trip: {
+          select: {
+            id: true,
+            name: true,
+            destination: true,
+          },
+        },
+        paidBy: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    if (expense === null) {
+      throw new NotFoundException(`Not found expense with ID: ${id}`);
+    }
+
+    return expense;
   }
 
-  update(id: number, updateExpenseDto: UpdateExpenseDto) {
-    return `This action updates a #${id} expense`;
+  async update(id: number, updateExpenseDto: UpdateExpenseDto) {
+    await this.findOne(id);
+
+    return this.database.expense.update({
+      where: { id },
+      data: updateExpenseDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} expense`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.database.expense.delete({
+      where: { id },
+    });
   }
 }
