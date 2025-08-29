a31cdf855cb664f2a91b8f0ec7a880347b7d99f7
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-08-30 00:11:36 +0200

    fix: remove unused import

--- src/participant/participant.service.ts
+++ src/participant/participant.service.ts
@@ -1,8 +1,4 @@
-import {
-  ConflictException,
-  Injectable,
-  NotFoundException,
-} from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 
 import { DatabaseService } from "../database/database.service";
 import { CreateParticipantDto } from "./dto/create-participant.dto";
