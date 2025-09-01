f5f57eaa91e9e46f4d0ed4613e8aa9a6ea811811
marie <marie@firefly14G10a.uk-net>
2025-09-01 23:23:19 +0200

    fix: prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
   const trip = await prisma.trip.create({
     data: {
-    participant_id: 1,
-    date_start: new Date("2025-08-08T12:34:56Z"),
+      participant_id: 1,
+      date_start: new Date('2025-08-08T12:34:56Z'),
     },
   });
 
   const participant = await prisma.participant.create({
     data: {
-      first_name: "Maria",
-      second_name: "Magdalena",
-      last_name: "Kowalska",
-      gender: "female",
+      first_name: 'Maria',
+      second_name: 'Magdalena',
+      last_name: 'Kowalska',
+      gender: 'female',
     },
   });
 
@@ -29,10 +29,10 @@ async function main() {
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
