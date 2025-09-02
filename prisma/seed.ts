a41deae59dd0f449810cd18f4c8d9537cbb64a9d
marie <marie@firefly14G10a.uk-net>
2025-09-02 19:02:30 +0200

    fix: prisma Finally!

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+//import { PrismaClient } from '../generated/prisma';
+import { Gender, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('Rozpoczynam seeding...');
+
+  const trp = await prisma.trip.create({
+    data: {
+      startDate: new Date('2025-08-01T12:34:56Z'),
+      participant_id: 1,
+    },
+  });
+
+  const part = await prisma.participant.create({
+    data: {
+      first_name: 'Jan',
+      second_name: 'K',
+      last_name: 'Kowalski',
+      gender: Gender.MALE,
+    },
+  });
+
+  const exp = await prisma.expense.create({
+    data: {
+      trip_id: 1,
+      price: 9999.99,
+    },
+  });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exitCode = 1;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
