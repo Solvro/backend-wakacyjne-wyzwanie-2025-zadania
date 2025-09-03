6722d0ab5ff4df552a9f16eab59ddd68f2a741b5
Kajta <kajtakamil1@gmail.com>
2025-09-03 17:02:44 +0200

    feat: fixinf mistakes

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+bootstrap().catch((err) => {
+  console.error("Error during bootstrap:", err);
+});
