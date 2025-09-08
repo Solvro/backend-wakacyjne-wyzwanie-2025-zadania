6031a34fe42089997af323a6383d8a0c12015ec5
loudsheep <loudsheep05@gmail.com>
2025-09-08 15:22:43 +0200

    feat: create participant and expense resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { DatabaseModule } from "./database/database.module";
+import { DatabaseService } from "./database/database.service";
+import { ExpenseModule } from "./expense/expense.module";
+import { ParticipantModule } from "./participant/participant.module";
+import { ParticipantsController } from "./participants/participants.controller";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ParticipantModule, DatabaseModule, ExpenseModule],
+  controllers: [AppController, ParticipantsController],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
