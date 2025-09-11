1b2629293f39c6a0b0ff45bcdb8134f87becf3e1
lolakk05 <k.stolar2005@gmail.com>
2025-09-11 16:56:48 +0200

    feat: unit test

--- src/participant/participant.controller.spec.ts
+++ src/participant/participant.controller.spec.ts
@@ -1,3 +1,8 @@
+import { Role } from "@prisma/client";
+import { AuthModule } from "src/auth/auth.module";
+import { DatabaseModule } from "src/database/database.module";
+import type { PaginationDto } from "src/pagination/pagination.dto";
+
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
 
@@ -7,16 +12,123 @@ import { ParticipantService } from "./participant.service";
 describe("ParticipantController", () => {
   let controller: ParticipantController;
 
+  const mockParticipantService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    findByEmail: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ParticipantController],
       providers: [ParticipantService],
-    }).compile();
+      imports: [AuthModule, DatabaseModule],
+    })
+      .overrideProvider(ParticipantService)
+      .useValue(mockParticipantService)
+      .compile();
 
     controller = module.get<ParticipantController>(ParticipantController);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  it("should create a new record", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockParticipantService.create.mockReturnValue(dto);
+
+    const result = await controller.create(dto);
+
+    expect(result).toEqual(dto);
+    expect(mockParticipantService.create).toHaveBeenCalledTimes(1);
+  });
+
+  it("should update an exisiting participant", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockParticipantService.create.mockReturnValue(dto);
+
+    const resultCreate = await controller.create(dto);
+
+    expect(resultCreate.email).toBe("janusz@example.com");
+
+    const dtoUpdate = {
+      name: "janek",
+    };
+
+    const updateParticipant = { ...dto, ...dtoUpdate };
+
+    mockParticipantService.update.mockReturnValue(updateParticipant);
+
+    const result = await controller.update(dto.participant_id, dtoUpdate);
+
+    expect(result.name).toBe("janek");
+    expect(mockParticipantService.update).toHaveBeenCalled();
+    expect(mockParticipantService.update).toHaveBeenCalledTimes(1);
+  });
+
+  it("should find one record by id", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockParticipantService.findOne.mockReturnValue(dto);
+
+    const result = await controller.findOne(1);
+
+    expect(result).toHaveProperty("participant_id", 1);
+    expect(mockParticipantService.findOne).toHaveBeenCalledTimes(1);
+    expect(mockParticipantService.findOne).toHaveBeenCalledWith(1);
+  });
+
+  it("should find many records", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+    const dto1 = {
+      participant_id: 2,
+      name: "Janusz2",
+      email: "janusz2@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+    mockParticipantService.findAll.mockReturnValue([dto, dto1]);
+    const paginationDto: PaginationDto = { limit: 10, skip: 0 };
+    const result = await controller.findAll(paginationDto);
+
+    expect(result).toHaveLength(2);
+    expect(mockParticipantService.findAll).toHaveBeenCalled();
+    expect(mockParticipantService.findAll).toHaveBeenCalledTimes(1);
   });
 });
--- src/participant/participant.service.spec.ts
+++ src/participant/participant.service.spec.ts
@@ -1,3 +1,7 @@
+import { Role } from "@prisma/client";
+import { DatabaseService } from "src/database/database.service";
+import type { PaginationDto } from "src/pagination/pagination.dto";
+
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
 
@@ -5,16 +9,181 @@ import { ParticipantService } from "./participant.service";
 
 describe("ParticipantService", () => {
   let service: ParticipantService;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let database: DatabaseService;
+
+  const mockDatabaseService = {
+    participant: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      findByEmail: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ParticipantService],
-    }).compile();
+      providers: [ParticipantService, DatabaseService],
+    })
+      .overrideProvider(DatabaseService)
+      .useValue(mockDatabaseService)
+      .compile();
 
     service = module.get<ParticipantService>(ParticipantService);
+    database = module.get<DatabaseService>(DatabaseService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should find one participant by id", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockDatabaseService.participant.findUnique.mockResolvedValue(dto);
+
+    const result = await service.findOne(1);
+
+    expect(result).toHaveProperty("participant_id", 1);
+    expect(mockDatabaseService.participant.findUnique).toHaveBeenCalledWith({
+      where: { participant_id: 1 },
+    });
+  });
+
+  it("should find all participants", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+    const dto1 = {
+      participant_id: 2,
+      name: "Janusz2",
+      email: "janusz2@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockDatabaseService.participant.findMany.mockResolvedValue([dto, dto1]);
+
+    const paginationDto: PaginationDto = { limit: 10, skip: 0 };
+    const result = await service.findAll(paginationDto);
+
+    expect(result).toHaveLength(2);
+    expect(result).toEqual([dto, dto1]);
+    expect(mockDatabaseService.participant.findMany).toHaveBeenCalled();
+  });
+
+  it("should find participant by email", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockDatabaseService.participant.findByEmail.mockResolvedValue(dto);
+
+    const result = await service.findOneByEmail("janusz@example.com");
+
+    expect(result?.email).toBe("janusz@example.com");
+  });
+
+  it("should create a participant", async () => {
+    const dto = {
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockDatabaseService.participant.create.mockResolvedValue({
+      participant_id: 1,
+      ...dto,
+    });
+
+    const result = await service.create(dto);
+
+    expect(result).toHaveProperty("participant_id");
+    expect(result.name).toBe("Janusz");
+    expect(await service.create(dto)).toEqual({
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    });
+  });
+
+  it("should delete record", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockDatabaseService.participant.delete.mockResolvedValue(dto);
+
+    const result = await service.remove(1);
+
+    expect(result).toHaveProperty("participant_id");
+    expect(mockDatabaseService.participant.delete).toHaveBeenCalledWith({
+      where: { participant_id: 1 },
+    });
+  });
+
+  it("should update participant", async () => {
+    const dto = {
+      participant_id: 1,
+      name: "Janusz",
+      email: "janusz@example.com",
+      password: "123",
+      role: Role.Participant,
+      isEnabled: true,
+    };
+
+    mockDatabaseService.participant.create.mockResolvedValue(dto);
+
+    const resultCreate = await service.create(dto);
+
+    expect(resultCreate).toHaveProperty("participant_id", 1);
+
+    const dtoUpdate = {
+      name: "janek",
+    };
+
+    const updatedParticipant = { ...dto, ...dtoUpdate };
+
+    mockDatabaseService.participant.update.mockResolvedValue(
+      updatedParticipant,
+    );
+
+    const result = await service.update(dto.participant_id, dtoUpdate);
+
+    expect(result.name).toBe("janek");
   });
 });
