7479a866ea257924301e432b2c6f93e54cb73add
reksion <janjuskowiak2004@gmail.com>
2025-09-16 16:57:43 +0200

    chore: add e2e for participant

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,6 @@
-import { AuthModule } from "src/auth/auth.module";
-
 import { Module, forwardRef } from "@nestjs/common";
 
+import { AuthModule } from "../auth/auth.module";
 import { DatabaseModule } from "../database/database.module";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
