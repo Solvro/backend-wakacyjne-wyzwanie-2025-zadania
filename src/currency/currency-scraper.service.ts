b3f7a705a29dd2d525725eece63f33345289e5fb
Harukume <harusume1001@gmail.com>
2025-09-18 14:31:46 +0200

    fix: eslint warning

--- src/currency/currency-scraper.service.ts
+++ src/currency/currency-scraper.service.ts
@@ -14,29 +14,25 @@ export class CurrencyScraperService {
 
     const currencies = await page.evaluate(() => {
       const rows = document.querySelectorAll(".rates-table__row");
-      return (
-        [...rows]
-          .map((row) => {
-            const nameElement = row.querySelector(
-              ".rates-table__exchange-rate",
-            );
-            const rateElement = row.querySelector("[data-rate-value]");
-            if (nameElement == null || rateElement == null) {
-              return null;
-            }
-            return {
-              name: nameElement.textContent.trim(),
-              rate: rateElement.textContent.trim(),
-            };
-          })
-          .filter(Boolean)
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          .filter((c) =>
-            ["EUR / PLN", "USD / PLN", "CHF / PLN", "GBP / PLN"].includes(
-              c!.name.replaceAll("\u00A0", " "),
-            ),
-          )
-      );
+      return [...rows]
+        .map((row) => {
+          const nameElement = row.querySelector(".rates-table__exchange-rate");
+          const rateElement = row.querySelector("[data-rate-value]");
+          if (nameElement == null || rateElement == null) {
+            return null;
+          }
+          return {
+            name: nameElement.textContent.trim(),
+            rate: rateElement.textContent.trim(),
+          };
+        })
+        .filter(Boolean)
+        .filter((c) =>
+          ["EUR / PLN", "USD / PLN", "CHF / PLN", "GBP / PLN"].includes(
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            c!.name.replaceAll("\u00A0", " "),
+          ),
+        );
     });
 
     console.warn(currencies);
